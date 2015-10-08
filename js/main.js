5265d8533ee7ecda11e85fc0352687308b65a609
Jesse Soldat <jsoldat@hotmail.com>
2015-10-08 15:29:52 -0400

    added live data to index.html

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 (function () {
-  console.log('It Works!');
-  console.dir(_);
-  console.dir($);
+  // console.log('It Works!');
+  // console.dir(_);
+  // console.dir($);
+  console.dir(etsy);
+  //-------------------------
+  var templateString = $('#itemTemplate').text();
+  var templateFunction = _.template(templateString);
+
+  _.each(etsy.results, function(item){
+    // console.log('price', item.price);
+
+    var itemHTML = templateFunction(item);
+
+    $('#mainContent').append(itemHTML);
+ 
+ 
+  });
+
 }());
+
+
